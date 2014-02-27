471361664d151133e06b5015ec2859e93f64501f
Mike Riley <dev+mike@blinqmedia.com>
2014-02-27 16:56:09 -0500

    Added README.md and a first draft of a generic template.
    --Added README.md for documentation purposes
    --Added a generic template
    --Tested that the template works (it did on my machine).

---
+++ README.md
@@ -0,0 +1,18 @@
+Rails Application Templates
+
+A generic Rails application template for use. Have to constantly go
+through steps to build applications with the technologies, gems and
+other configuration files that I use.  Simplifying it with a Rails
+application template.
+
+It will do the following:
+a) Assume you are using rspec instead of test::unit
+b) Assume you are using MySQL
+c) Assume you are using Git for your source control
+d) Assume you are using autotest with Growl
+e) Ask if you want to download bootstrap and unpack it for you
+f) Assume you will use FactoryGirl instead of fixtures
+g) Install pry for debugging
+h) Create a default .gitignore file
+
+Review the template to be sure it is going to do what you want.
---
+++ generic_template.rb
@@ -0,0 +1,49 @@
+gem_group :development do  
+  gem 'rspec-rails'
+  gem 'pry'
+end
+
+gem_group :test do
+  gem 'faker'
+  gem 'capybara'
+  gem 'guard-rspec'
+  gem 'ZenTest', :require => false  
+  gem 'autotest-rails'
+  gem 'autotest-growl'
+  gem 'autotest-fsevent'  
+  gem 'launchy'
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+end
+
+run "cat << EOF >> .gitignore
+/.bundle
+/db/*.sqlite3
+/db/*.sqlite3-journal
+log/*
+log/**/*
+tmp/*
+tmp/**/*
+database.yml
+doc/
+*.swp
+*.swo
+*~
+.DS_Store
+public/uploads/*
+*.orig
+EOF"
+
+if yes?("Download bootstrap?")
+  run "wget https://github.com/twbs/bootstrap/archive/v3.0.0.zip -O bootstrap.zip -O bootstrap.zip"
+  run "unzip bootstrap.zip -d bootstrap && rm bootstrap.zip"
+  run "cp bootstrap/bootstrap-3.0.0/dist/css/bootstrap.css vendor/assets/stylesheets/"
+  run "cp bootstrap/bootstrap-3.0.0/dist/js/bootstrap.js vendor/assets/javascripts/"
+  run "rm -rf bootstrap"
+  run "echo '@import \"bootstrap\";' >>  app/assets/stylesheets/application.css.scss"
+  run "rails g simple_form:install --bootstrap"
+end
+
+git :init
+git add: "."
+git commit: %Q{ -m 'Initial commit' }
